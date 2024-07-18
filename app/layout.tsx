21e3b869119d3276d15142c656377c88d7043442
Chukwunwike Udezue <udezuec@protonmail.com>
2024-07-18 11:46:03 +0100

    Chore: Code refactoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,9 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} min-h-screen w-screen antialiased bg-background`}
-      >
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
