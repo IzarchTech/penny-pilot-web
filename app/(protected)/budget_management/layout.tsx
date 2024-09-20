c5875e4d60a70d571037a340bd8c473202942fe9
Chukwunwike Udezue <udezuec@protonmail.com>
2024-09-20 03:19:30 +0100

    Feat: Budgement management layout

---
+++ app/(protected)/budget_management/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Budget Management - Penny Pilot",
+};
+
+/**
+ * A wrapper component for the protected budget management pages.
+ *
+ * This component is used to wrap all the pages under the "budget management"
+ * section. It provides a common layout for all those pages.
+ *
+ * @param {{ children: React.ReactNode }} props The props object.
+ * @param {React.ReactNode} props.children The children of the component.
+ */
+export default function BudgetManagementLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <div className="container">{children}</div>;
+}
