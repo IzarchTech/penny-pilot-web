4a68f75a6e93a08ee8aedf2dac86fd9cf4f4e224
Chukwunwike Udezue <udezuec@protonmail.com>
2026-04-11 16:44:20 +0100

    feat: protected layout and sidebar

--- src/routes/(protected)/-components/sidebar.tsx
+++ src/routes/(protected)/-components/sidebar.tsx
@@ -1,21 +1,44 @@
 import { Link } from "@tanstack/react-router";
-import { LayoutDashboardIcon } from "lucide-react";
+import { InfoIcon, LayoutDashboardIcon, LogOutIcon, ScrollTextIcon, SettingsIcon } from "lucide-react";
 import Flex from "#/components/flex";
 import styles from "./sidebar.module.css";
 
 export default function Sidebar() {
 	return (
 		<Flex as="nav" direction="column" className={styles.sidebar}>
-			<SidebarItem />
+			<Flex direction="column" className={styles.mainNav}>
+				<SidebarItem to="/overview" label="Overview" icon={<LayoutDashboardIcon />} />
+				<SidebarItem to="/transactions" label="Transactions" icon={<ScrollTextIcon />} />
+				<SidebarItem to="/settings" label="Settings" icon={<SettingsIcon />} />
+			</Flex>
+			<Flex direction="column">
+				<SidebarItem to="/support" label="Support" icon={<InfoIcon />} />
+				<button type="button" className={styles.logoutButton}>
+					<LogOutIcon />
+					<span>Logout</span>
+				</button>
+			</Flex>
 		</Flex>
 	);
 }
 
-function SidebarItem() {
+type SidebarItemProps = {
+	to: string;
+	label: string;
+	icon: React.ReactNode;
+};
+
+function SidebarItem({ to, label, icon }: Readonly<SidebarItemProps>) {
 	return (
-		<Link to="/overview" className={styles.sidebarItem}>
-			<LayoutDashboardIcon />
-			<span>Overview</span>
+		<Link
+			to={to}
+			className={styles.sidebarItem}
+			activeProps={{
+				className: styles.active,
+			}}
+		>
+			{icon}
+			<span>{label}</span>
 		</Link>
 	);
 }
